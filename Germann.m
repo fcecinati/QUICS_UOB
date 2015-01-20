41480f3e6f9e115124fba4816a32e386f78e521f
fcecinati <fcecinati@gmail.com>
2015-01-20 18:35:45 +0000

    Completed temporal and spatial correlation tests

--- Germann.m
+++ Germann.m
@@ -146,29 +146,43 @@ hold on
 % plot(plx,ply1,'r')
 plot(plx,ply2,'g')
 legend('error correlation coefficients',str2)
-saveas(error_corr,'error_correlation','fig')
+saveas(error_corr,'error_spatial_correlation','fig')
 hold off
 close(error_corr)
 
-% %% temporal decorrelation function
-% time_corr = zeros(x,2);
-% 
-% for j=1:t-1
-%     for k=2:t
-%         
-% 
-% 
-% 
-% % Noooooooooo.... problem with nan and consecutive values!
-% time_corr = zeros(x,3);
-% for j=1:x
-%     vector=err(:,j);
-%     vector(isnan(vector))=[];
-%     time_corr(j,:)=autocorr(vector,2);
-% end
+%% temporal decorrelation function
+rho_t = zeros(x,2);
 
+for j=1:x
+    v1 = err(1:t-1,j);
+    v2 = err(2:t,j);
+    m1 = nanmean(v1);
+    m2 = nanmean(v2);
+    sd1 = nanstd(v1);
+    sd2 = nanstd(v2);
+    rho_t(j,1)=(nanmean((v1-m1).*(v2-m2)))/(sd1*sd2);
+end
 
+for j=1:x
+    v1 = err(1:t-2,j);
+    v2 = err(3:t,j);
+    m1 = nanmean(v1);
+    m2 = nanmean(v2);
+    sd1 = nanstd(v1);
+    sd2 = nanstd(v2);
+    rho_t(j,2)=(nanmean((v1-m1).*(v2-m2)))/(sd1*sd2);
+end
 
+rho = nanmean(rho_t);
+x_axis = [1 2];
+
+rho_fig = figure;
+figure(rho_fig)
+plot(x_axis,rho)
+title('temporal correlation of the errors');
+xlabel('hours')
+saveas(rho_fig,'error_temporal_correlation','fig')
+close(rho_fig)
 
 %% SVD Decomposition
 
@@ -218,82 +232,88 @@ L = U*(D^0.5);
 % end
 % r2m = nanmean(r2);
 % 
-% % AR(2) parameters
-% a1 = r1m*(1-r2m)/(1-(r1m^2));
-% a2 = (r2m-(r1m^2))/(1-(r1m^2));
-% 
-% % Scaling factor
-% v=((1+a2)/((1-a2)*(1-a1+a2)*(1+a1+a2)))^(-0.5);
-% 
-%% Generation of the perturbed field
+% Instead, taking the r1 and r2 from temporal decorrelation data
+r1m = rho(1);
+r2m = rho(2);
 
+% AR(2) parameters
+a1 = r1m*(1-r2m)/(1-(r1m^2));
+a2 = (r2m-(r1m^2))/(1-(r1m^2));
+
+% Scaling factor
+v=((1+a2)/((1-a2)*(1-a1+a2)*(1+a1+a2)))^(-0.5);
+
+%% Generation of the perturbed field
 n = number_ensembles;
-% delta1 = zeros(x,t,n);
+delta1 = zeros(x,t,n);
 delta = zeros(x,t,n);
 
 for ne = 1:n
-%     delta1(:,1,ne)= L*normrnd(0,1,x,1);
-%     delta1(:,2,ne)= L*normrnd(0,1,x,1);
-    for tm = 1:t
-%         delta1(:,tm,ne) = L*normrnd(0,1,x,1) + a1*delta1(:,tm-1,ne) + a2*delta1(:,tm-2,ne);
-%         delta(:,tm,ne) = delta1(:,tm,ne)*v + mean_e;
-          delta(:,tm,ne) = L*normrnd(0,1,x,1)+ mean_e';
+    delta1(:,1,ne)= L*normrnd(0,1,x,1);
+    delta1(:,2,ne)= L*normrnd(0,1,x,1);
+    for tm = 3:t
+        delta1(:,tm,ne) = L*normrnd(0,1,x,1) + a1*delta1(:,tm-1,ne) + a2*delta1(:,tm-2,ne);
+        delta(:,tm,ne) = delta1(:,tm,ne).*v + mean_e';
+%         delta(:,tm,ne) = L*normrnd(0,1,x,1)+ mean_e';
     end 
 end
 
 % decorrelation test on generated delta
-d1 = delta(:,:,1)';
-mean_d = nansum(R.*d1)./nansum(R);
-diff = zeros(t,x);
-for i=1:t 
-    diff(i,:) = d1(i,:)- mean_d;
-end
-cov_d = zeros(x,x);
-temp1 = zeros(t,1);
+for f=1:n
+    d1 = delta(:,:,f)';
+    mean_d = nansum(R.*d1)./nansum(R);
+    diff = zeros(t,x);
+    for i=1:t 
+        diff(i,:) = d1(i,:)- mean_d;
+    end
+    cov_d = zeros(x,x);
+    temp1 = zeros(t,1);
 
-for k = 1:x
-    for l = 1:x
-        for tm = 1:t
-            temp1(tm) = diff(tm,k)*diff(tm,l);
+    for k = 1:x
+        for l = 1:x
+            for tm = 1:t
+                temp1(tm) = diff(tm,k)*diff(tm,l);
+            end
+            cov_d(k,l) = nansum(temp1)/t;
         end
-        cov_d(k,l) = nansum(temp1)/t;
     end
-end
 
-correl_d = zeros(x,x);
+    correl_d = zeros(x,x);
 
-for x1=1:x
-    for x2=1:x
-        correl_d(x1,x2) = cov_d(x1,x2)/((cov_d(x1,x1)*cov_d(x2,x2))^0.5);
+    for x1=1:x
+        for x2=1:x
+            correl_d(x1,x2) = cov_d(x1,x2)/((cov_d(x1,x1)*cov_d(x2,x2))^0.5);
+        end
     end
+
+    c = reshape(correl_d,x*x,1);
+    d = reshape(distance,x*x,1);
+    d(isnan(c)) = [];
+    c(isnan(c)) = [];
+    dmax = max(max(d));
+
+    [fitobjectinv_d, gofinv_d] = fit(d,c,'rat11');
+    coeffi =  coeffvalues(fitobjectinv_d);
+    rsq = gofinv_d.rsquare;
+    plx =(0:dmax/100:dmax);
+    ply3=((coeffi(1)*plx) + coeffi(2))./(plx+coeffi(3));
+    str3 = ['(' num2str(coeffi(1)) '*x+' num2str(coeffi(2)) ')/(x+' num2str(coeffi(3)) ')   R square = ' num2str(rsq)];
+
+    error_corr_d = figure;
+    figure(error_corr_d)
+    scatter(d,c)
+    title(['spatial correlation of the generated errors' num2str(f)]);
+    xlabel('meters')
+    hold on
+    % plot(plx,ply1,'r')
+    plot(plx,ply3,'g')
+    plot(plx,ply2,'r')
+    legend('generated error correlation coefficients','fitting curve','fitting curve of the original error spatial correlation')
+    name = ['error_correlation_d' num2str(f)];
+    saveas(error_corr_d,name,'fig')
+    hold off
+    close(error_corr)
 end
-
-c = reshape(correl_d,x*x,1);
-d = reshape(distance,x*x,1);
-d(isnan(c)) = [];
-c(isnan(c)) = [];
-dmax = max(max(d));
-
-[fitobjectinv_d, gofinv_d] = fit(d,c,'rat11');
-coeffi =  coeffvalues(fitobjectinv_d);
-rsq = gofinv_d.rsquare;
-plx =(0:dmax/100:dmax);
-ply2=((coeffi(1)*plx) + coeffi(2))./(plx+coeffi(3));
-str2 = ['(' num2str(coeffi(1)) '*x+' num2str(coeffi(2)) ')/(x+' num2str(coeffi(3)) ')   R square = ' num2str(rsq)];
-
-error_corr_d = figure;
-figure(error_corr_d)
-scatter(d,c)
-title('spatial correlation of the generated errors');
-xlabel('meters')
-hold on
-% plot(plx,ply1,'r')
-plot(plx,ply2,'g')
-legend('generated error correlation coefficients',str2)
-saveas(error_corr_d,'error_correlation_d','fig')
-hold off
-close(error_corr)
-
 
 
 
