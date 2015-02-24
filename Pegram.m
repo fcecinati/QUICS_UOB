fcd3950fb5684fe4557e127c166c08d4d539dc18
fcecinati <fcecinati@gmail.com>
2015-02-24 14:32:40 +0000

    Not averaged error calibration

--- Pegram.m
+++ Pegram.m
@@ -7,229 +7,399 @@ rng('shuffle')
 
 %% Parameters to define:
 
-% Path to the folder where the radar data is 
-data_path = '\\ads.bris.ac.uk\filestore\MyFiles\Staff3\fc14509\Documents\QUICS\MATLAB_rep\data_north_england\09-2008';
-% file to work on
-filename = 'metoffice-c-band-rain-radar_uk_200809051700_1km-composite.dat.gz';
-% Temporary folder
+%Paths:
+
+% Path to the folder where the radar data is:
+data_path = '\\ads.bris.ac.uk\filestore\MyFiles\Staff3\fc14509\Documents\QUICS\MATLAB_rep\data_north_england\05-2008';
+% Temporary folder:
 tmpfolder = '\\ads.bris.ac.uk\filestore\MyFiles\Staff3\fc14509\Documents\QUICS\MATLAB_rep\Temp\'; 
-% Script folder
-script_folder = '\\ads.bris.ac.uk\filestore\MyFiles\Staff3\fc14509\Documents\QUICS\MATLAB_rep\QUICS_UOB';
-% Results folder
-results_folder = '\\ads.bris.ac.uk\filestore\MyFiles\Staff3\fc14509\Documents\QUICS\MATLAB_rep\Results\Test_Pegram\Test_6\';
+% Results folder:
+results_folder = '\\ads.bris.ac.uk\filestore\MyFiles\Staff3\fc14509\Documents\QUICS\MATLAB_rep\Results\Test_Pegram\Test_11\';
+% Brief description for this test:
+description = 'This test is for the calibration part. it should return a plot of autocorrelation to decide which noise threshold to use.';
+
+% Parameters and information:
+
+% Starting date and time:
+starting = '2008-05-05 15:00:00';
+% Ending date and time:
+ending = '2008-05-05 16:30:00';
+% Time step in minutes:
+tstep = 5;
 % How many ensembles do you want to generate
 number_ens = 5;
-% Resolution in km
-res = 1;
+% You need to work on a square area with a multiple of 2 number of pixels.
+% what are the easting and nothing in meters (top left corner)?
+easting = 300000;
+northing = 556000;
+% What is the number of pixels for the square side?
+side = 256;
 
-%% Data loading and pre-processing
+% Calibration mode (Y/N):
+cm = 'Y';
+% if not in calibration mode, what is the wavelenght threshold between
+% signal and noise in km?
+threshold = 8;
+% if in calibration mode, which wavelenghts you want to try?
+testWL = [4 6 8 10 12];
+%   how many time steps you want to test the autocorrelation on (it must be
+%   smallet than the total timesteps tsteps)?
+calibsteps = 12;
 
+%% Script
+
+tic
+
+% Results folder
 if ~exist(results_folder, 'dir')
   mkdir(results_folder);
 end
+cd(results_folder)
+Readme = fopen('README.txt', 'w');
+fprintf(Readme, description);
+fclose(Readme);
 
-cd(data_path)
-tt=length(filename);
-if (strcmp('.gz', filename(tt-2:tt)) == 1)   % if the input file is compressed (i.e. with extension '.gz')
-     newname = gunzip(filename, tmpfolder); % decompress file in a temporary folder and output new file name
-     filename = newname{1,1};    % copy new name into the nimrodfilename
-end
-cd(script_folder)
-[radar, date, param] = readnimrod(filename);
-
-radar(radar<0.5) = NaN;
-m = nanmean(nanmean(radar));
-lograd = log(radar);
-Z = lograd - m;
-
-% trim at a multiple of 2 size
-x = (param(3):1000:param(3)+(param(7)*1000 -1000));
-y = (param(2):-1000:(param(2)-(param(6)*1000)+1000));
-indx = (x>350000&x<478000);
-indy = (y>768000&y<896000);
-R = Z(indy,indx);
-
-sz = size(R); sz = sz(1);
-
-% Creates a rain/norain mask
-mask = zeros(size(R));
-mask(isnan(R)==0)=1;
-
-R(isnan(R)==1) = 0;
-
-%% Pegram method
-
-threshold = 8;      %km wavelenght
-
-% Set frequencies and wavelength
-fx = (1/res).*(-(sz/2):(sz/2)); fx = (fx(2:end)-0.5)./sz; %km^-1
-fy = fx;
-[FX, FY] = meshgrid (fx, fy);
-F = (FX.^2 + FY.^2).^0.5;
-WL = 1./F;                               % km
-WLlog = log(WL);
-
-% Fast Fourier Transform
-RF = fft2(R);     RF = fftshift(RF);
-P = (abs(RF)).^2;
-phase = angle(RF);
-Plog = log(P);
-
-% Line fitting and Ns identification
-Pcenter = P(WL>(threshold-3)&WL<(threshold+3));
-WLcenter = WL(WL>(threshold-3)&WL<(threshold+3));
-Pc = log(Pcenter);
-WLc = log(WLcenter);
-linearCoef = polyfit(WLc,Pc,1);
-Pline = WLlog.*linearCoef(1) + linearCoef(2);
-Plineexp = exp(Pline);
-ns = log(threshold)*linearCoef(1) + linearCoef(2); ns = exp(ns);
-
-% Plotting
-xh = [1 max(WL(:))]; yh = [ns ns];
-xv = [threshold threshold]; yv = [0.1 max(P(:))];
-fig=figure;
-figure(fig)
-scatter(WL(:),P(:),'k','.')
-set(fig, 'Position', [200 200 1400 700])
-set(gca, 'XScale','log')
-set(gca, 'YScale','log')
-hold on
-scatter(WLcenter(:),Pcenter(:),'r','.')
-plot(WL(:),Plineexp(:))
-plot(xh,yh,'--k')
-plot(xv,yv,'--k')
-saveas (fig, [results_folder, 'logP.jpg'])
-close(fig)
-
-% Separation of Signal and Noise
-
-Ps = NaN(sz,sz);
-Pn = NaN(sz,sz);
-Ps(P>ns) = P(P>ns);
-Pn(P<ns) = P(P<ns);
-[Pavg, rad] = nanradialavg(P, (sz/2));
-[Psavg, ~] = nanradialavg(Ps, (sz/2));
-[Pnavg, ~] = nanradialavg(Pn, (sz/2));
-Pavg(1) = Pavg(2); Psavg(1) = Psavg(2); Pnavg(1) = Pnavg(2);
-
-fr = rad./2;
-wl = 1./fr;
-fig=figure;
-figure(fig)
-plot(wl,Pavg,'k')
-set(fig, 'Position', [200 200 1400 700])
-set(gca, 'XScale','log')
-set(gca, 'YScale','log')
-hold on
-plot(wl,Psavg,'b')
-plot(wl,Pnavg,'r')
-saveas (fig, [results_folder, 'avgP.jpg'])
-close(fig)
-
-position = round(F.*sz);
-SF = zeros(sz,sz);
-
-for x=1:sz
-    for y=1:sz
-        if position(x,y)<=(sz/2)
-            SF(x,y) = RF(x,y) * (Psavg(position(x,y))/Pavg(position(x,y)));
+if cm~='Y'
+    % Dates:
+    s_date = datenum(starting);
+    e_date = datenum(ending);
+    tst = (1/1440)*tstep;     %transforms the tstep unit in days
+    t=0;
+    
+    for time = s_date:tst:e_date
+        
+        t=t+1;
+        
+        % File to work on
+        datename = [sprintf('%4i',year(time)),sprintf('%02i',month(time)),sprintf('%02i',day(time)),sprintf('%02i',hour(time)),sprintf('%02i',minute(time))];
+        filename = ['metoffice-c-band-rain-radar_uk_',datename,'_1km-composite.dat.gz'];
+        cd(data_path)
+        tt = length(filename);
+        if (strcmp('.gz', filename(tt-2:tt)) == 1)   % if the input file is compressed (i.e. with extension '.gz')
+             newname = gunzip(filename, tmpfolder); % decompress file in a temporary folder and output new file name
+             filename = newname{1,1};    % copy new name into the nimrodfilename
         end
-    end
-end
+        currentfullpath = mfilename('fullpath'); filenamesz = (size(mfilename)); currentdir = currentfullpath(1:(end-filenamesz(2)));
+        cd(currentdir)        
+        [radar, date, param] = readnimrod(filename);
+        
+        % Data preprocessing
+        radar(radar<0.5) = NaN;
+        lograd = log(radar);
+        m = nanmean(nanmean(lograd));
+        Z = lograd - m;
 
-% Generate the random noise for the ensembles
-NF = zeros(sz,sz,number_ens);
-for en=1:number_ens
-    WF = normrnd(0,1,sz,sz);
-    for x=1:sz
-        for y=1:sz
-            if position(x,y)<=(sz/2)
-                if x>y
-                    NF(x,y,en) = WF(x,y) * (Pnavg(position(x,y)).^0.5);   
-                else
-                    NF(x,y,en) = WF(y,x) * (Pnavg(position(x,y)).^0.5);
-                end
-            else
-                if x>y
-                    NF(x,y,en) = WF(x,y) * (Pnavg(sz/2).^0.5);   
-                else
-                    NF(x,y,en) = WF(y,x) * (Pnavg(sz/2).^0.5);
+        % Trim at a multiple of 2 size
+        x = (param(3):param(5):param(3)+(param(7)*param(5)-param(5)));
+        y = (param(2):-param(4):(param(2)-(param(6)*param(4))+param(4)));
+        res = param(4)/1000;    % in km
+        eastingmax = easting + side*param(5);
+        northingmin = northing - side*param(4);
+        indx = (x>easting&x<eastingmax);
+        indy = (y>northingmin&y<northing);
+        R = Z(indy,indx);
+        sz = side;
+
+        % Creates a rain/norain mask
+        mask = zeros(size(R));
+        mask(isnan(R)==0)=1;
+        
+        % Substitute NaNs with zeros for the fft
+        R(isnan(R)==1) = 0;
+        
+        %% Pegram method
+
+        % Set frequencies and wavelength
+        fx = (1/res).*(-(sz/2):(sz/2)); fx = (fx(2:end)-0.5)./sz; %km^-1
+        fy = fx;
+        [FX, FY] = meshgrid (fx, fy);
+        F = (FX.^2 + FY.^2).^0.5;
+        WL = 1./F;                               % km
+        WLlog = log(WL);
+
+        % Fast Fourier Transform
+        RF = fft2(R);     RF = fftshift(RF);
+        P = (abs(RF)).^2;
+        Plog = log(P);
+        
+        % Line fitting and Ns identification
+        Pcenter = P(WL>(threshold-3)&WL<(threshold+3));
+        WLcenter = WL(WL>(threshold-3)&WL<(threshold+3));
+        Pc = log(Pcenter);
+        WLc = log(WLcenter);
+        linearCoef = polyfit(WLc,Pc,1);
+        Pline = WLlog.*linearCoef(1) + linearCoef(2);
+        Plineexp = exp(Pline);
+        ns = log(threshold)*linearCoef(1) + linearCoef(2); ns = exp(ns);
+
+        % Separation of Signal and Noise
+        Ps = NaN(sz,sz);
+        Pn = NaN(sz,sz);
+        Ps(P>ns) = P(P>ns);
+        Pn(P<ns) = P(P<ns);
+        [Pavg, rad] = nanradialavg(P, (sz/2));
+        [Psavg, ~] = nanradialavg(Ps, (sz/2));
+        [Pnavg, ~] = nanradialavg(Pn, (sz/2));
+        Pavg(1) = Pavg(2); Psavg(1) = Psavg(2); Pnavg(1) = Pnavg(2);
+
+        % Create signal component in the frequency domain
+        position = round(F.*sz);
+        SF = zeros(sz,sz);
+        for x=1:sz
+            for y=1:sz
+                if position(x,y)<=(sz/2)
+                    SF(x,y) = RF(x,y) * (Psavg(position(x,y))/Pavg(position(x,y)));
                 end
             end
         end
+
+        % Create noise component in the frequency domain
+        NF = zeros(sz,sz,number_ens);
+        for en=1:number_ens
+            WF = normrnd(0,1,sz,sz);
+            for x=1:sz
+                for y=1:sz
+                    if position(x,y)<=(sz/2)
+                        if x>y
+                            NF(x,y,en) = WF(x,y) * (Pnavg(position(x,y)).^0.5);   
+                        else
+                            NF(x,y,en) = WF(y,x) * (Pnavg(position(x,y)).^0.5);
+                        end
+                    else
+                        if x>y
+                            NF(x,y,en) = WF(x,y) * (Pnavg(sz/2).^0.5);   
+                        else
+                            NF(x,y,en) = WF(y,x) * (Pnavg(sz/2).^0.5);
+                        end
+                    end
+                end
+            end
+        end
+        
+        % Generate the ensemble
+        EF = zeros(sz,sz,number_ens);
+        for en=1:number_ens
+            EF(:,:,en) = SF(:,:) + NF(:,:,en);
+        end
+
+        % Inverse the Fourier transform
+        E = zeros(sz,sz,number_ens);
+        for en=1:number_ens
+            EF(:,:,en) = fftshift(EF(:,:,en));
+            E(:,:,en) = ifft2(EF(:,:,en), 'symmetric');   
+            E(:,:,en) = exp(E(:,:,en) + m).* mask;
+        end
+        E(E<0) = 0;
+        
+        toc
+        
+        % Print out the ensembles and the radar
+        Rain =exp(R + m).*mask;
+
+        cmap = colormap('jet');
+        n = size(cmap, 1);
+        zerovalue = [1 1 1];
+        minimum = 0.0001;
+        maximum = max(max(log(Rain(:))), max(log(E(:))));
+        dmap = (maximum-minimum)/n;
+        for en = 1:number_ens
+            fig=figure;
+            figure(fig)
+            imagesc(log(E(:,:,en)), [0 maximum])
+            set(fig, 'Position', [200 200 800 800])
+            colormap([zerovalue;cmap]);
+            caxis([minimum-dmap maximum]);
+            colorbar
+            saveas (fig, [results_folder, 'ensemble',num2str(en),'_',datename,'.jpg'])
+            close(fig)
+        end
+        fig=figure;
+        figure(fig)
+        imagesc(log(Rain), [0 maximum])
+        set(fig, 'Position', [200 200 800 800])
+        colormap([zerovalue;cmap]);
+        caxis([minimum-dmap maximum]);
+        colorbar
+        saveas (fig, [results_folder, 'radar','_',datename,'.jpg'])
+        close(fig)
+        
+        toc
+        
+        save([results_folder,'Ensemble_',datename,'.mat'])
+        
+        toc
+    
     end
-end
+    
+%     for  time = s_date:tstep:e_date
+%         plotfilename = [results_folder,'Ensemble_',datename,'.mat'];
+%         PlotPower(plotfilename)
+%     end
+    
+    toc
+else
+    
+    ntests = size(testWL,2);
+    autocorrel = zeros(calibsteps,ntests);
+    
+    for test=1:ntests
+        
+        threshold = testWL(test);
+        
+        % Dates:
+        s_date = datenum(starting);
+        e_date = datenum(ending);
+        tst = (1/1440)*tstep;     %transforms the tstep unit in days
 
-% Generate the ensemble
-EF = zeros(sz,sz,number_ens);
-for en=1:number_ens
-    EF(:,:,en) = SF(:,:) + NF(:,:,en);
-end
+        toc 
+        t=0;
+        for time = s_date:tst:e_date
+            
+            t=t+1;
 
-% Inverse the Fourier transform
-E = zeros(sz,sz,number_ens);
-for en=1:number_ens
-    EF(:,:,en) = fftshift(EF(:,:,en));
-    E(:,:,en) = ifft2(EF(:,:,en), 'symmetric');   
-    E(:,:,en) = exp(E(:,:,en) + m).* mask;
-end
-E(E<0) = 0;
+            % File to work on
+            datename = [sprintf('%4i',year(time)),sprintf('%02i',month(time)),sprintf('%02i',day(time)),sprintf('%02i',hour(time)),sprintf('%02i',minute(time))];
+            filename = ['metoffice-c-band-rain-radar_uk_',datename,'_1km-composite.dat.gz'];
+            cd(data_path)
+            tt = length(filename);
+            if (strcmp('.gz', filename(tt-2:tt)) == 1)   % if the input file is compressed (i.e. with extension '.gz')
+                 newname = gunzip(filename, tmpfolder); % decompress file in a temporary folder and output new file name
+                 filename = newname{1,1};    % copy new name into the nimrodfilename
+            end
+            currentfullpath = mfilename('fullpath'); filenamesz = (size(mfilename)); currentdir = currentfullpath(1:(end-filenamesz(2)));
+            cd(currentdir)        
+            [radar, date, param] = readnimrod(filename);
 
-% Print out the ensembles and the radar
-Rain =exp(R + m).*mask;
+            % Data preprocessing
+            radar(radar<0.5) = NaN;
+            lograd = log(radar);
+            m = nanmean(nanmean(lograd));
+            Z = lograd - m;
 
-maximum = max(max(Rain(:)), max(E(:)));
-for en = 1:number_ens
-    fig=figure;
+            % Trim at a multiple of 2 size
+            x = (param(3):param(5):param(3)+(param(7)*param(5)-param(5)));
+            y = (param(2):-param(4):(param(2)-(param(6)*param(4))+param(4)));
+            res = param(4)/1000;    % in km
+            eastingmax = easting + side*param(5);
+            northingmin = northing - side*param(4);
+            indx = (x>easting&x<eastingmax);
+            indy = (y>northingmin&y<northing);
+            R = Z(indy,indx);
+            sz = side;
+
+            % Creates a rain/norain mask
+            masktemp = zeros(size(R));
+            masktemp(isnan(R)==0)=1;
+            mask(:,:,t) = masktemp;
+
+            % Substitute NaNs with zeros for the fft
+            R(isnan(R)==1) = 0;
+
+            %% Pegram method
+
+            % Set frequencies and wavelength
+            fx = (1/res).*(-(sz/2):(sz/2)); fx = (fx(2:end)-0.5)./sz; %km^-1
+            fy = fx;
+            [FX, FY] = meshgrid (fx, fy);
+            F = (FX.^2 + FY.^2).^0.5;
+            WL = 1./F;                               % km
+            WLlog = log(WL);
+
+            % Fast Fourier Transform
+            RF = fft2(R);     RF = fftshift(RF);
+            P = (abs(RF)).^2;
+            Plog = log(P);
+
+            % Line fitting and Ns identification
+            Pcenter = P(WL>(threshold-3)&WL<(threshold+3));
+            WLcenter = WL(WL>(threshold-3)&WL<(threshold+3));
+            Pc = log(Pcenter);
+            WLc = log(WLcenter);
+            linearCoef = polyfit(WLc,Pc,1);
+            Pline = WLlog.*linearCoef(1) + linearCoef(2);
+            Plineexp = exp(Pline);
+            ns = log(threshold)*linearCoef(1) + linearCoef(2); ns = exp(ns);
+
+            % Separation of Signal and Noise
+            Ps = NaN(sz,sz);
+            Pn = NaN(sz,sz);
+            Ps(P>ns) = P(P>ns);
+            Pn(P<ns) = P(P<ns);
+            [Pavg, rad] = nanradialavg(P, (sz/2));
+            [Psavg, ~] = nanradialavg(Ps, (sz/2));
+            [Pnavg, ~] = nanradialavg(Pn, (sz/2));
+            Pavg(1) = Pavg(2); Psavg(1) = Psavg(2); Pnavg(1) = Pnavg(2);
+
+            % Create signal component in the frequency domain
+            position = round(F.*sz);
+            SF = zeros(sz,sz);
+            for x=1:sz
+                for y=1:sz
+                    if position(x,y)<=(sz/2)
+                        SF(x,y) = RF(x,y) * (Psavg(position(x,y))/Pavg(position(x,y)));
+                    end
+                end
+            end
+
+            NF(:,:,t) = RF - SF;
+            
+            N = zeros(sz,sz,t);
+            for tm=1:t
+                NF(:,:,tm) = fftshift(NF(:,:,tm));
+                N(:,:,tm) = ifft2(NF(:,:,tm), 'symmetric');   
+                N(:,:,tm) = (exp(N(:,:,tm))).*mask(:,:,tm);
+            end
+                      
+            toc
+        end
+
+        toc
+        
+        timesteps = size(N,3);
+        mu = mean(N,3);
+        sigma = std(N,0,3);
+
+        diff = zeros(sz,sz,timesteps);
+        
+        for tm=1:timesteps
+            diff(:,:,tm) = N(:,:,tm) - mu;
+        end
+
+        for lag=1:calibsteps
+            diff_t1 = diff(:,:,1:(end-lag));
+            diff_t2 = diff(:,:,(1+lag):end);
+            temp = zeros(sz,sz,t-lag);
+            for tm=1:timesteps-lag
+                temp(:,:,tm) = diff_t1(:,:,tm).*diff_t2(:,:,tm);
+            end
+            ac = mean(temp, 3)./ sigma;
+            autocorrel(lag, test) = nanmean(nanmean(ac));
+        end
+        
+        toc
+
+    end
+    
+    toc
+    
+    save ([results_folder,'calibrationtest'])
+    
+    timeline = (1:12).*tstep;
+    linecolours = colormap('jet');
+    fig = figure;
     figure(fig)
-    imagesc(E(:,:,en), [0 maximum])
-    set(fig, 'Position', [200 200 800 800])
-    colorbar
-    saveas (fig, [results_folder, 'ensemble',num2str(en),'.jpg'])
-    close(fig)
+    hold on
+    for test = 1:ntests
+        plot(timeline',autocorrel(:,test),'color',linecolours(floor((64/ntests)*test),:))
+    end
+    saveas(fig,[results_folder,'autocorrelation.jpg'])
+    
+    toc
+    
 end
 
-
-fig=figure;
-figure(fig)
-imagesc(Rain, [0 maximum])
-set(fig, 'Position', [200 200 800 800])
-colorbar
-saveas (fig, [results_folder, 'radar.jpg'])
-close(fig)
+    
+        
+        
     
 
-%% EMD and Fourier transform
-
-% IMS = bemd(R);
-% N = IMS(:,:,1);
-% S = IMS(:,:,4)+IMS(:,:,2)+IMS(:,:,3);
-% 
-% N = N.*mask;
-% S = S.*mask;
-% 
-% NF = fft2(N);       NF = fftshift(NF);
-% SF = fft2(S);       SF = fftshift(SF);
-% 
-% PN = (abs(NF)).^2;
-% PS = (abs(SF)).^2;
-% 
-% [radavg, ~] = radialavg(P, (sz/2));
-% [noiseavg, ~] = radialavg(PN, (sz/2));
-% [signalavg, ~] = radialavg(PS, (sz/2));
-% 
-% f = (1:(sz/2))./(sz/2); % km^-1
-% wl = 2*pi./f;           % km
-% 
-% fig=figure;
-% figure(fig)
-% loglog(wl, radavg,'k')
-% hold on
-% loglog(wl, noiseavg, 'r')
-% loglog(wl, signalavg, 'b')
-% set(fig, 'Position', [0 0 1400 1000])
-% legend('total field','noise','signal')
-% saveas(fig,[results_folder,'components.jpg'])
-% 
- save([results_folder, 'Test16'])
